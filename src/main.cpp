4387aa50438bd7e23440ff6187d8b50f3195121d
vvlade <vvlade24@gmail.com>
2024-01-23 19:38:39 +0100

    Code changes made for/due to Gcov reports

--- src/main.cpp
+++ src/main.cpp
@@ -2,8 +2,8 @@
 #include <iostream>
 #include <string>
 
-#include "GLFW/glfw3.h"
 #include "glad/glad.h"
+#include "GLFW/glfw3.h"
 #include "glm/glm.hpp"
 #include "glm/gtc/matrix_transform.hpp"
 #include "glm/gtc/type_ptr.hpp"
